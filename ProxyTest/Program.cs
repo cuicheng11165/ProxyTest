f782083c6bc668b05e53ab0416579c24ac2e55fd
Simmon Cui <chengcui@avepoint.com>
2023-10-31 16:24:02 +0800

    Add Sample

--- ProxyTest/Program.cs
+++ ProxyTest/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace ProxyTest
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Titanium.Web.Proxy;
+using Titanium.Web.Proxy.EventArguments;
+using Titanium.Web.Proxy.Http;
+using Titanium.Web.Proxy.Models;
+namespace ProxyTest
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static void Main(string[] args)
+        {
+         
+
+        }
+
+
+
     }
-  }
 }
